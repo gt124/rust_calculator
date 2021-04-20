dd59981346ce81ef7707adb976dfdc39fa51bfa0
galen <galentaylor@gmail.com>
2021-04-19 18:59:29 -0700

    got add to work with basic strings.

--- src/big_calc.rs
+++ src/big_calc.rs
@@ -10,6 +10,9 @@ impl BigCalc {
     fn testy(&self){
         println!("it worked");
     }
+    fn add(&self, left: &'static str, right: &'static str) -> i32{
+        return left.parse::<i32>().unwrap() + right.parse::<i32>().unwrap() ;
+    }
 
 }
 
@@ -19,12 +22,12 @@ mod tests {
     use super::*;
 
     #[test]
-    fn internal() {
-
-
+    fn add_test() {
         let sut = BigCalc::new();
         //let sut = BigCalc{};
         sut.testy();
+        let output = sut.add("22", "33");
+        println!("{}", output);
         //assert_eq!(1, value_in_cents(penny));
     }
 }
