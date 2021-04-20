b36681986e0360f8083e42bad31980fcf1ba59b2
galen <galentaylor@gmail.com>
2021-04-20 14:44:26 -0700

    got add to work with basic strings.

--- src/big_calc.rs
+++ src/big_calc.rs
@@ -12,6 +12,10 @@ impl BigCalc {
     }
     fn add(&self, left: &'static str, right: &'static str) -> i32{
         return left.parse::<i32>().unwrap() + right.parse::<i32>().unwrap() ;
+        //pad the strings with 0's
+    }
+    fn pad_to_same_length(&self, left: &'static str, right: &'static str ) -> (String, String) {
+        return (String::from("test"), String::from("other"));
     }
 
 }
@@ -29,5 +33,10 @@ mod tests {
         let output = sut.add("22", "33");
         println!("{}", output);
         //assert_eq!(1, value_in_cents(penny));
+    }
+
+    #[test]
+    fn pad_to_same_length_test(){
+
     }
 }
